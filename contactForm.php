bdb61d03846c6a2c117e23e474cc3a5ee218e10a
OmniOmni <austin.olijar@gmail.com>
2022-05-15 11:18:33 -0500

    Updates

--- contactForm.php
+++ contactForm.php
@@ -2,12 +2,28 @@
 
 
     $to = "austin@austinolijar.com";   
-	$firstName = $_POST['firstName'];
+	$firstName = $_POST["first-name"];
+	$lastName = $_POST["last-name"];
+    $title = $_POST["title"];
+    $email = $_POST["email"];
+    $businessName = $_POST["business-name"];
+    $country = $_POST["country"];
+    $businessYears = $_POST["years-in-business"];
+    $businessAddress = $_POST["business-address"];
+    $websiteURL = $_POST["website-url"];
+    $productInquiry = $_POST["products"];
+    $comments = $_POST["comments"];
     
-    mail($to, "New Contact Form Submission", $firstName);
+    $products = json_encode($productInquiry)
+	
+	$txt = "A new inquiry from Elevate Oral Care's International Site. The following information was submitted. \n\n First Name: $firstName \n Last Name: $lastName \n Title: $title \n Email: $email \n Business Name: $businessName \n Country: $country \n Years In Business: $businessYears \n Business Address: $businessAddress \n Website: $websiteURL \n $products \n Comments: $comments";
+	
+    $contactFrom = "From: Elevate Oral Care International Inquiry"
+    
+    mail($to, "New Contact Form Submission", $txt, $contactFrom);
 
 
-    header("Location: http://thisisatesttesttest.com/mail-sent");
+    header("Location: http://thisisatesttesttest.com/#/mail-sent");
 
     exit();
 
