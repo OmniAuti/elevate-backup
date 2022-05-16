fa1ee089cfe4ddfa8842a6a3e81e7cf0b6e49e4f
OmniOmni <austin.olijar@gmail.com>
2022-05-16 08:26:32 -0500

    Updates

--- contactForm.php
+++ contactForm.php
@@ -5,7 +5,7 @@
 	$firstName = $_POST["first-name"];
 	$lastName = $_POST["last-name"];
     $title = $_POST["title"];
-    $email = $_POST["email"];
+    $email = $_POST["email-true"];
     $businessName = $_POST["business-name"];
     $country = $_POST["country"];
     $businessYears = $_POST["years-in-business"];
@@ -14,6 +14,9 @@
     $productInquiry = $_POST["products"];
     $comments = $_POST["comments"];
     
+    if(!empty($_POST['email'])) die();
+
+
     $products = json_encode($productInquiry)
 	
 	$txt = "A new inquiry from Elevate Oral Care's International Site. The following information was submitted. \n\n First Name: $firstName \n Last Name: $lastName \n Title: $title \n Email: $email \n Business Name: $businessName \n Country: $country \n Years In Business: $businessYears \n Business Address: $businessAddress \n Website: $websiteURL \n $products \n Comments: $comments";
