24f887a547bb69ed695451ef2654e53c5ea80255
OmniOmni <austin.olijar@gmail.com>
2022-05-16 09:09:17 -0500

    Updates

--- contactForm.php
+++ contactForm.php
@@ -16,12 +16,11 @@
     
     if(!empty($_POST['email'])) die();
 
-
-    $products = json_encode($productInquiry)
+    $products = json_encode($productInquiry);
 	
 	$txt = "A new inquiry from Elevate Oral Care's International Site. The following information was submitted. \n\n First Name: $firstName \n Last Name: $lastName \n Title: $title \n Email: $email \n Business Name: $businessName \n Country: $country \n Years In Business: $businessYears \n Business Address: $businessAddress \n Website: $websiteURL \n $products \n Comments: $comments";
 	
-    $contactFrom = "From: Elevate Oral Care International Inquiry"
+    $contactFrom = "From: Elevate Oral Care International Inquiry";
     
     mail($to, "New Contact Form Submission", $txt, $contactFrom);
 
